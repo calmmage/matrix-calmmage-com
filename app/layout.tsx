d53d80de8b312aa64c0bfc5b935b77d46305cb78
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-10-04 02:15:45 +0300

    Add website icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,23 @@ export const metadata: Metadata = {
   title: 'Matrix Stormwave',
   description: 'Interactive Matrix background with dynamic effects',
   generator: 'Matrix Stormwave',
+  icons: {
+    icon: [
+      { url: '/icon_sets/M3d/favicon.ico' },
+      // { url: '/icon_sets/pixel3d/favicon.ico' },
+      // { url: '/icon_sets/spiral3d/favicon.ico' },
+      // { url: '/icon_sets/toroid3d/favicon.ico' },
+
+      { url: '/icon_sets/M3d/icon.svg', type: 'image/svg+xml' },
+      // { url: '/icon_sets/pixel3d/icon.svg', type: 'image/svg+xml' },
+      // { url: '/icon_sets/spiral3d/icon.svg', type: 'image/svg+xml' },
+      // { url: '/icon_sets/toroid3d/icon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/icon_sets/M3d/icon-180.png',
+    // apple: '/icon_sets/pixel3d/icon-180.png',
+    // apple: '/icon_sets/M3d/spiral3d-180.png',
+    // apple: '/icon_sets/M3d/toroid3d-180.png',
+  },
 }
 
 export default function RootLayout({
